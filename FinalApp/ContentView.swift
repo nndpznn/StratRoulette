14cb1dc74941ce5d575384b4dffd3cfb2faad68b
Nolan Nguyen! <nolan2087@gmail.com>
2024-04-03 12:42:16 -0700

    Initial Commit

---
+++ FinalApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FinalApp
+//
+//  Created by Nolan Nguyen on 4/3/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FinalApp/FinalAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinalAppApp.swift
+//  FinalApp
+//
+//  Created by Nolan Nguyen on 4/3/24.
+//
+
+import SwiftUI
+
+@main
+struct FinalAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
